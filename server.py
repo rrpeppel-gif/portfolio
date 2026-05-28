3b17f2b475cc556308dc0c5d7cc2fc8731dd872a
Robin van de Peppel <robin.van.de.peppel@hva.nl>
2026-05-28 12:55:27 +0200

    fix flask startup

--- server.py
+++ server.py
@@ -26,8 +26,6 @@ def punishing():
 def contact():
     return render_template("contact.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
